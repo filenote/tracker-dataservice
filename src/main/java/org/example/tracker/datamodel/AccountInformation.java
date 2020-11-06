c97a302b3b6a9a68de17f58c144f06b00ea189b4
Pedro Rodriguez <prodriguez.eh@gmail.com>
2020-11-06 14:42:39 -0600

    account information

---
+++ src/main/java/org/example/tracker/datamodel/AccountInformation.java
@@ -0,0 +1,9 @@
+package org.example.tracker.datamodel;
+
+import lombok.Data;
+
+@Data
+public class AccountInformation {
+    private String username;
+    private String email;
+}
--- src/main/java/org/example/tracker/service/AccountService.java
+++ src/main/java/org/example/tracker/service/AccountService.java
@@ -1,12 +1,14 @@
 package org.example.tracker.service;
 
 import com.google.common.collect.ImmutableList;
+import org.example.tracker.datamodel.AccountInformation;
 import org.example.tracker.datamodel.Role;
 import org.example.tracker.datamodel.SimplerGrantedAuthority;
 import org.example.tracker.datamodel.UserCredentials;
 import org.example.tracker.exception.UsernameAlreadyExists;
 import org.example.tracker.repository.UserCredentialsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +34,14 @@ public class AccountService {
         credentials.setPassword(encodedPassword);
         credentials.setAuthorities(List.copyOf(defaultRoles));
         return repository.addAccount(credentials);
+    }
+
+    public AccountInformation getAccountInformation(Authentication authentication) {
+        UserCredentials userCredentials = repository.findUserCredentialsByUsername(authentication.getName());
+        AccountInformation information = new AccountInformation();
+        information.setUsername(userCredentials.getUsername());
+        information.setEmail(null);
+        return information;
     }
 }
 
